6df580023ef5d5ab1abdc8cd89f59fd3b8a6e42a
Kristiyan96 <kristiyan.c@gmail.com>
2019-10-25 08:50:03 +0300

    Optimize landing for mobile devices

--- src/views/Guest/Landing/Landing.vue
+++ src/views/Guest/Landing/Landing.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="landing">
-    <v-container>
+    <v-container class="hidden-sm-and-down">
       <v-row>
         <v-col md="5" sm="5" xs="12" class="col-left">
           <h1 class="mb-3">Групово и индивидуално <br>обучение по математика</h1>
@@ -16,8 +16,26 @@
           </v-btn>
         </v-col>
         <v-col md="7" sm="7" xs="12">
-          <img class="bg-image" src="@/assets/images/bg.png"/>
+          <img class="bg-image d-sm-none d-md-flex" src="@/assets/images/bg.png"/>
         </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="hidden-md-and-up">
+      <v-row>
+        <img class="bg-image-small" src="@/assets/images/bg.png"/>
+      </v-row>
+      <v-row class="main-small justify-center">
+        <h1 class="mt-3 mb-3">Групово и индивидуално <br>обучение по математика</h1>
+        <v-btn 
+          class="primary" 
+          rounded 
+          x-large
+          depressed
+          href="#contacts"
+          v-smooth-scroll="{ offset: -70}"
+        >
+          Прегледай курсовете
+        </v-btn>
       </v-row>
     </v-container>
   </section>
@@ -44,6 +62,8 @@ export default {
     text-align: center
   .bg-image
     height: calc(100vh - 64px)
+  .bg-image-small
+    width: 100%
 h1
   font-size: 2em
 </style>
