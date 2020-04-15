953cc53affcaa245fdb87d7a9b4ffa93b79a6587
Kristiyan96 <kristiyan.c@gmail.com>
2020-04-15 04:06:32 +0300

    Fix payments search

--- src/views/Payments/Payments.vue
+++ src/views/Payments/Payments.vue
@@ -1,8 +1,14 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col xs="12" md="5">
-        <v-card class="pb-0" outlined>
+      <v-col
+        xs="12"
+        md="5"
+      >
+        <v-card
+          class="pb-0"
+          outlined
+        >
           <v-card-title>
             <span class="headline">Плащания</span>
             <v-spacer></v-spacer>
@@ -18,7 +24,10 @@
           <v-card-text class="px-0 pb-0">
             <v-container class="px-0 pb-0">
               <v-row>
-                <v-col cols="12" class="px-0 pb-0">
+                <v-col
+                  cols="12"
+                  class="px-0 pb-0"
+                >
                   <v-data-table
                     v-if="filtered_payments.length"
                     :headers="headers"
@@ -27,20 +36,29 @@
                     class="table-scroll"
                   >
                     <template v-slot:item="{ item }">
-                      <tr @click="activate(item)" :class="{active: payment && item.id == payment.id}">
+                      <tr
+                        @click="activate(item)"
+                        :class="{active: payment && item.id == payment.id}"
+                      >
                         <td>{{ item.student.name }}</td>
                         <td>{{ item.amount }}</td>
                       </tr>
                     </template>
                   </v-data-table>
-                  <div v-else class="px-3 grey--text py-3">Няма плащания</div>
+                  <div
+                    v-else
+                    class="px-3 grey--text py-3"
+                  >Няма плащания</div>
                 </v-col>
               </v-row>
             </v-container>
           </v-card-text>
         </v-card>
       </v-col>
-      <v-col xs="12" md="7">
+      <v-col
+        xs="12"
+        md="7"
+      >
         <PaymentPreview :payment="payment" />
       </v-col>
     </v-row>
@@ -63,14 +81,14 @@ export default {
       search: "",
       headers: [
         {
-          text: 'Име',
-          align: 'left',
+          text: "Име",
+          align: "left",
           sortable: false,
-          value: 'name',
+          value: "name"
         },
-        { text: 'Сума', value: 'amount' }
-      ],
-    }
+        { text: "Сума", value: "amount" }
+      ]
+    };
   },
   mounted() {
     store.dispatch(FETCH_PAYMENTS);
@@ -80,27 +98,29 @@ export default {
       this.payment = item;
     },
     capitalize(s) {
-      if (typeof s !== 'string') return ''
-      return s.charAt(0).toUpperCase() + s.slice(1)
+      if (typeof s !== "string") return "";
+      return s.charAt(0).toUpperCase() + s.slice(1);
     }
   },
   computed: {
     ...mapGetters(["payments"]),
     filtered_payments() {
-      return this.payments.filter(p =>
-        p && p.student && ( 
-        p.student.name.toLowerCase().search(this.search.toLowerCase()) != -1 || 
-        p.month.toLowerCase().search(this.search.toLowerCase()) != -1));
+      return this.payments.filter(
+        p =>
+          p &&
+          p.student &&
+          p.student.name.toLowerCase().search(this.search.toLowerCase()) != -1
+      );
     }
   },
   watch: {
     payments() {
-      if(this.payments.length) {
+      if (this.payments.length) {
         this.payment = this.payments[0];
       }
     }
   }
-}
+};
 </script>
 
 <style lang="sass">
