5004cac875d0b1313214c5156b2deb18428e355a
Kristiyan96 <kristiyan.c@gmail.com>
2020-05-30 16:03:34 +0300

    Sending confirmation email

--- src/views/Accounts/ForgotPassword.vue
+++ src/views/Accounts/ForgotPassword.vue
@@ -1,40 +1,80 @@
 <template>
-  <v-container fluid class="fill-height">
+  <v-container
+    fluid
+    class="fill-height"
+  >
     <v-row
       align="center"
       justify="center"
     >
-      <v-col xs="10" md="4">
+      <v-col
+        xs="10"
+        md="4"
+      >
         <v-card shaped>
           <v-card-title class="text-center mb-4">
             <div class="text-center display-1">{{$t("auth.title.forgot_password")}}</div>
           </v-card-title>
 
           <v-card-text>
-            <v-alert :value="failure" color="error" class="mb-4" outlined>
+            <v-alert
+              :value="failure"
+              color="error"
+              class="mb-4"
+              outlined
+            >
               {{ response }}
             </v-alert>
-            <form @submit.prevent="submit" class="pt-5">
+            <form
+              @submit.prevent="submit"
+              class="pt-5"
+            >
               <v-text-field
                 v-model="email"
                 :label="$t('auth.label.email')"
                 type="email"
                 outlined
-              ><font-awesome-icon :icon="['fa', 'at']" slot="prepend-inner"/>
+              >
+                <font-awesome-icon
+                  :icon="['fa', 'at']"
+                  slot="prepend-inner"
+                />
               </v-text-field>
               <div class="text-center">
-                <v-btn large color="primary" @click="submit" :class="{ disabled: success }">
+                <v-btn
+                  large
+                  color="primary"
+                  @click="submit"
+                  :class="{ disabled: success }"
+                >
                   {{ $t('auth.button.change_password') }}
                 </v-btn>
               </div>
               <!-- The following line submits the form when pressing enter -->
-              <input type="submit" value="Submit" class="d-none" />
+              <input
+                type="submit"
+                value="Submit"
+                class="d-none"
+              />
             </form>
           </v-card-text>
           <v-card-actions>
-            <v-layout align-space-between justify-space-between row fill-height>
-              <v-btn text rounded to="/login">{{ $t("auth.link.signin") }}</v-btn>
-              <v-btn text rounded to="/register">{{ $t("auth.link.register") }}</v-btn>
+            <v-layout
+              align-space-between
+              justify-space-between
+              row
+              fill-height
+            >
+              <v-btn
+                text
+                rounded
+                to="/login"
+              >{{ $t("auth.link.signin") }}</v-btn>
+              <v-btn
+                text
+                rounded
+                to="/register"
+              >{{ $t("auth.link.register") }}</v-btn>
             </v-layout>
           </v-card-actions>
         </v-card>
@@ -65,7 +105,7 @@ export default {
       }
 
       this.axios
-        .post("/api/password", {
+        .post("/password", {
           user: {
             email: this.email
           }
