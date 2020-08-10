710c26dd31aa5e9190a136ba79c111e9602e5c8f
Kristiyan96 <kristiyan.c@gmail.com>
2020-08-10 10:52:47 +0300

    Update profile description

--- src/views/Accounts/Settings/TeacherProfile.vue
+++ src/views/Accounts/Settings/TeacherProfile.vue
@@ -3,11 +3,26 @@
     <template v-slot:title>
       Моята обява
     </template>
-
+    <template v-slot:content>
+      <v-textarea
+        class="mb-2"
+        v-model="user.description"
+        label="Моля, опишете Вашия опит, постижения..."
+        :rules="descriptionRules"
+        required
+      ></v-textarea>
+      <SkillsList />
+    </template>
     <template v-slot:actions>
       <v-spacer></v-spacer>
-      <v-btn color="primary" depressed>
-        Редактирай
+      <v-btn
+        color="primary"
+        :disabled="!dirty"
+        depressed
+        :loading="loading"
+        @click="submit"
+      >
+        Запази
       </v-btn>
     </template>
   </LayoutColumn>
@@ -16,25 +31,58 @@
 <script>
 import { mapGetters } from "vuex";
 import store from "@/store";
-import { FETCH_SUBJECTS, FETCH_LEVELS } from "@/store/actions.type";
+import {
+  FETCH_SUBJECTS,
+  FETCH_LEVELS,
+  FETCH_SKILLS,
+  UPDATE_PROFILE,
+  FETCH_PROFILE
+} from "@/store/actions.type";
 import LayoutColumn from "@/layout/LayoutColumn";
+import SkillsList from "./TeacherSkills/SkillsList";
 
 export default {
   components: {
-    LayoutColumn
+    LayoutColumn,
+    SkillsList
   },
   data() {
     return {
-      show: false
+      show: false,
+      user: {},
+      loading: false,
+      descriptionRules: [v => !!v || "Описанието е задължително."]
     };
   },
   mounted() {
     store.dispatch(FETCH_SUBJECTS);
     store.dispatch(FETCH_LEVELS);
+    store.dispatch(FETCH_SKILLS, this.current_user.id);
+    this.user = {
+      ...this.current_user
+    };
   },
-  methods: {},
+  methods: {
+    submit() {
+      this.loading = true;
+      store.dispatch(UPDATE_PROFILE, this.user).then(response => {
+        this.loading = false;
+      });
+    }
+  },
   computed: {
-    ...mapGetters(["current_user", "subjects", "levels"])
+    ...mapGetters(["current_user", "subjects", "levels"]),
+    dirty() {
+      return this.user.description != this.current_user.description;
+    }
+  },
+  watch: {
+    current_user: {
+      immediate: true,
+      handler() {
+        store.dispatch(FETCH_PROFILE, { id: null });
+      }
+    }
   }
 };
 </script>
