40f7c7acb6290eb7a1e0b56a71c672ba0b03aa5f
Kristiyan96 <kristiyan.c@gmail.com>
2021-03-06 17:17:07 +0200

    Create a TeacherProducts component

---
+++ src/views/Account/TeacherProfile/TeacherProducts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="root">
+    <ProductList />
+    <v-btn>
+      <font-awesome-icon icon="plus" class="grey--text mr-3" />
+      {{ $t('account.ad.addSubject') }}
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import ProductList from '@/views/Products/ProductList'
+
+export default {
+  components: {
+    ProductList
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass"></style>
