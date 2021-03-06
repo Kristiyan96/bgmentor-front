89bd2adadf2cf7e1a1968d2f09f40a5ea5d8f84d
Kristiyan96 <kristiyan.c@gmail.com>
2021-03-06 17:16:16 +0200

    Create a form wrapper component

---
+++ src/components/FormWrapper.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <div class="alert alert-info" v-if="notice">{{ notice }}</div>
+    <div class="alert alert-danger" v-if="error">{{ error }}</div>
+
+    <v-main>
+      <slot />
+    </v-main>
+
+    <v-btn
+      type="submit"
+      large
+      color="primary"
+      class="btn mt-3"
+      v-if="!actions.length"
+    >
+      {{ $t('actions.submit') }}</v-btn
+    >
+
+    <v-btn
+      v-for="action in actions"
+      :key="action.label"
+      large
+      :color="action.color ?? 'primary'"
+      class="btn mt-3"
+    >
+      {{ $t(action.label) }}</v-btn
+    >
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+      description: 'Title of the form, using t18 string'
+    },
+    actions: {
+      type: Array,
+      default: () => [],
+      description: 'A list of action buttons. Expects label & color'
+    },
+    onSubmit: {
+      type: Function,
+      default: () => {},
+      description: 'Action to be performed when the form is submitted'
+    },
+    notice: {
+      type: String,
+      default: '',
+      description: 'Important information about the form or submition.'
+    },
+    error: {
+      type: String,
+      default: '',
+      description: 'Error messages from submition.'
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
