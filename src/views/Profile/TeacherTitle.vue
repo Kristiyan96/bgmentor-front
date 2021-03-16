b6cf1f759f970999f9b28716c1e27456313c833f
Kristiyan96 <kristiyan.c@gmail.com>
2021-03-17 01:37:52 +0200

    Use a dialog form to edit user title

--- src/views/Profile/TeacherTitle.vue
+++ src/views/Profile/TeacherTitle.vue
@@ -1,43 +1,34 @@
 <template>
   <div class="my-1 mb-3 h5 grey--text d-flex align-center">
-    <v-btn
-      @click="editing = true"
-      color="primary"
-      v-if="!editing"
-      icon
-      smallclass="mr-5"
-    >
+    <v-btn @click="openDialog" color="primary" icon smallclass="mr-5">
       <font-awesome-icon icon="edit" />
     </v-btn>
-    <span v-if="!editing">{{ profile.title }}</span>
-    <span class="d-flex" v-else>
+    <span>{{ profile.title }}</span>
+    <DialogForm
+      :open="open"
+      :onSubmit="saveTitle"
+      @onClose="closeDialog"
+      title="Update your title"
+      :error="error"
+    >
       <v-text-field
         v-model="title"
         :label="$t(`profile.labels.title`)"
       ></v-text-field>
-      <v-btn @click="cancelEditing" class="mr-2 mt-2" icon>
-        <font-awesome-icon icon="times" />
-      </v-btn>
-      <v-btn
-        @click="saveTitle"
-        color="primary"
-        class="mt-2"
-        :loading="loading"
-        icon
-      >
-        <font-awesome-icon icon="check" />
-      </v-btn>
-    </span>
+    </DialogForm>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import store from '@/store'
+import DialogForm from '@/components/DialogForm'
 
 export default {
   name: 'TeacherTitle',
-  components: {},
+  components: {
+    DialogForm
+  },
   props: {
     teacher: {
       type: Object
@@ -47,13 +38,21 @@ export default {
     return {
       loading: false,
       title: '',
-      editing: false
+      open: false,
+      error: null
     }
   },
   mounted() {
-    this.title = this.profile.title
+    this.resetEditing()
   },
   methods: {
+    openDialog() {
+      this.open = true
+    },
+    closeDialog() {
+      this.open = false
+      this.resetEditing()
+    },
     async saveTitle() {
       this.loading = true
 
@@ -63,20 +62,19 @@ export default {
           title: this.title
         })
         this.error = null
-        this.editing = false
+        this.closeDialog()
       } catch (error) {
         this.error = error
       } finally {
         this.loading = false
       }
     },
-    cancelEditing() {
-      this.editing = false
+    resetEditing() {
       this.title = this.profile.title
     }
   },
   computed: {
-    ...mapGetters(['currentUser']),
+    ...mapGetters(['currentUser', 'profile']),
     editable() {
       return this.profile.id === this.currentUser.id
     }
