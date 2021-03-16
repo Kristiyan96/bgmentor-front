e7458487928ac2aaa139cf884ed65d50990c9d4d
Kristiyan96 <kristiyan.c@gmail.com>
2021-03-17 01:04:14 +0200

    Create a teacher tags section

---
+++ src/views/Profile/TeacherTagsSection.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card class="mx-auto mt-4" flat>
+    <v-card-title>
+      {{ $t(`profile.labels.${model}`) }}
+      <v-spacer />
+      <AddTag v-if="editable" :model="model" />
+    </v-card-title>
+    <v-card-text>
+      <v-chip-group active-class="deep-purple accent-4 white--text" column>
+        <v-chip
+          v-for="m in profile[modelList]"
+          :key="m"
+          class="mr-2"
+          color="primary"
+          :close="editable"
+          @click:close="deleteModel(m)"
+        >
+          {{ m }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import store from '@/store'
+import DialogForm from '@/components/DialogForm'
+import AddTag from '@/views/Profile/AddTeacherTag'
+
+export default {
+  name: 'TeacherTags',
+  props: {
+    model: {
+      type: String,
+      description: 'Which user field to edit. Has to be an array of tags'
+    }
+  },
+  components: {
+    DialogForm,
+    AddTag
+  },
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    async deleteModel(m) {
+      this.loading = true
+      const filteredList = this.currentUser[this.modelList]
+        .filter((s) => s !== m)
+        .join(', ')
+
+      try {
+        await store.dispatch('updateProfile', {
+          [this.modelList]: filteredList
+        })
+        this.error = null
+      } catch (error) {
+        this.error = error
+      } finally {
+        this.loading = false
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['currentUser', 'profile']),
+    editable() {
+      return this.profile.id === this.currentUser.id
+    },
+    modelList() {
+      return `${this.model}_list`
+    }
+  }
+}
+</script>
