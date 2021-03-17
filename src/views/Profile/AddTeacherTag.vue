78dd4b4bb1682268ada18e439c48ce8f859274bb
Kristiyan96 <kristiyan.c@gmail.com>
2021-03-17 11:11:42 +0200

    Add rules to teacher tags

--- src/views/Profile/AddTeacherTag.vue
+++ src/views/Profile/AddTeacherTag.vue
@@ -14,6 +14,7 @@
     >
       <v-text-field
         v-model="modelValue"
+        :rules="modelRules"
         :label="$t(`profile.labels.${model}`)"
         :placeholder="$t(`profile.titles.${model}`)"
         outlined
