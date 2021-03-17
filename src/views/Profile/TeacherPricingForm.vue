77a300947ad7e8645b3206f5bbe662c292294910
Kristiyan96 <kristiyan.c@gmail.com>
2021-03-17 11:11:10 +0200

    Create teacher pricing form

---
+++ src/views/Profile/TeacherPricingForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <span class="h5 font-weight-bold">
+    <v-btn @click="openDialog" color="primary" icon smallclass="mr-5">
+      <font-awesome-icon icon="edit" />
+    </v-btn>
+    <DialogForm
+      :open="open"
+      :onSubmit="saveTitle"
+      @onClose="closeDialog"
+      :title="$t(`profile.titles.title`)"
+      :error="error"
+    >
+      <div v-for="pricing in pricings" :key="pricing.id" class="d-flex">
+        <v-text-field
+          :rules="minutesRules"
+          v-model="title"
+          :label="$t(`profile.labels.pricing_minutes`)"
+        ></v-text-field>
+        <v-text-field
+          :rules="priceRules"
+          v-model="title"
+          :label="$t(`profile.labels.pricing_price`)"
+        ></v-text-field>
+      </div>
+      <v-btn @click="addPricing" color="primary"> Add pricing </v-btn>
+    </DialogForm>
+  </span>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import store from '@/store'
+import DialogForm from '@/components/DialogForm'
+
+export default {
+  name: 'TeacherPricing',
+  components: {
+    DialogForm
+  },
+  props: {
+    teacher: {
+      type: Object,
+      default: () => {},
+      description: 'Teacher being displayed'
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      open: false,
+      error: null,
+      pricings: [],
+      minutesRules: [(v) => !!v || this.$t(`profile.rules.minutes`)],
+      priceRules: [(v) => !!v || this.$t(`profile.rules.price`)]
+    }
+  },
+  methods: {
+    addPricing() {
+      this.pricings.push({ ...this.newPricing })
+    },
+    openDialog() {
+      this.open = true
+    },
+    closeDialog() {
+      this.open = false
+      this.resetEditing()
+    },
+    async savePricing() {
+      this.loading = true
+
+      try {
+        await store.dispatch(
+          this.pricing.id ? 'updatePricing' : 'createPricing',
+          {
+            id: this.profile.id,
+            title: this.title
+          }
+        )
+        this.error = null
+        this.closeDialog()
+      } catch (error) {
+        this.error = error
+      } finally {
+        this.loading = false
+      }
+    },
+    resetEditing() {
+      this.pricings = this.profile.pricings
+    }
+  },
+  computed: {
+    ...mapGetters(['currentUser', 'profile', 'newPricing']),
+    editable() {
+      return this.profile.id === this.currentUser.id
+    }
+  }
+}
+</script>
