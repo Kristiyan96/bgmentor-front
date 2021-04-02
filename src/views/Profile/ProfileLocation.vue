fb578b3048842865f0b02efdb7cc71a01b061099
Kristiyan96 <kristiyan.c@gmail.com>
2021-04-02 14:25:06 +0300

    Remove error

--- src/views/Profile/ProfileLocation.vue
+++ src/views/Profile/ProfileLocation.vue
@@ -32,12 +32,12 @@ export default {
       return this.profile.id === this.currentUser.id
     },
     city() {
-      return this.profile.city_list.length
+      return this.profile.city_list && this.profile.city_list.length
         ? this.$t(`search.city.${this.profile.city_list[0]}`)
         : 'Unknown'
     },
     country() {
-      return this.profile.country_list.length
+      return this.profile.country_list && this.profile.country_list.length
         ? this.$t(`search.country.${this.profile.country_list[0]}`)
         : 'Unknown'
     }
