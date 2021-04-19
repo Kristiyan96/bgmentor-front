3ff74275a6b1da8222782f378d2612e93380000f
Kristiyan96 <kristiyan.c@gmail.com>
2021-04-19 12:44:51 +0300

    Prepend api for requests to rails server

--- src/backend/axios/config.js
+++ src/backend/axios/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://bgmentor.com/'
+    ? 'https://bgmentor.com/api'
     : 'http://localhost:3000/'
 export default API_URL
