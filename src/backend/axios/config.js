dd02c4d26f37a3809a9902e6e7cdb2a68eef832e
Kristiyan96 <kristiyan.c@gmail.com>
2021-04-20 04:09:36 +0300

    Test config.js

--- src/backend/axios/config.js
+++ src/backend/axios/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://bgmentor.com/api'
-    : 'http://localhost:3000/'
+    ? 'https://bgmentor.com/'
+    : 'http://127.0.0.1:3000/api/'
 export default API_URL
