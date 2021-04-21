92099216ab6c553b6f348acb6e9ce1dac790b49f
Kristiyan96 <kristiyan.c@gmail.com>
2021-04-21 22:21:11 +0300

    Fix API request config

--- src/backend/axios/config.js
+++ src/backend/axios/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === 'production'
     ? 'https://bgmentor.com/api'
-    : 'http://127.0.0.1:3000/api/'
+    : 'http://127.0.0.1:3000/api'
 export default API_URL
