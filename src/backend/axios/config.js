42cf45315c7cb1b25a0752bfcb17d5efc33d8400
Kristiyan96 <kristiyan.c@gmail.com>
2021-04-23 20:14:41 +0300

    Revert config.js change

--- src/backend/axios/config.js
+++ src/backend/axios/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:3000/api'
-// process.env.NODE_ENV === 'production'
-//   ? 'https://bgmentor.com/api'
-//   : 'http://localhost:3000/api'
+export const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://bgmentor.com/api'
+    : 'http://localhost:3000/api'
 export default API_URL
