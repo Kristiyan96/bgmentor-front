570d16c11b6e3bc2fb35f3acf75b65c0b7d14f2b
Kristiyan96 <kristiyan.c@gmail.com>
2021-04-26 10:52:01 +0300

    Improve footer

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,17 +1,25 @@
 <template>
-  <v-footer color="primary lighten-1 pt-3 mt-3" padless>
+  <v-footer color="primary lighten-1 pt-3 mt-3 pb-5" padless>
     <v-layout justify-center row wrap>
-      <ChooseLanguage />
-      <v-divider class="mx-4" vertical></v-divider>
+      <div class="bg-mentor">
+        <ChooseLanguage />
+      </div>
+      <br /><br />
       <v-btn color="white" class="my-2" text rounded to="/terms">
         Условия
       </v-btn>
       <v-btn color="white" class="my-2" text rounded to="/privacy">
         Поверителност
       </v-btn>
-      <v-flex primary lighten-2 py-3 class="text-center" white--text xs12>
-        &copy;{{ new Date().getFullYear() }} <strong>Бг Ментор</strong>
-      </v-flex>
+      <div
+        py-3
+        class="text-center white--text bg-mentor primary lighten-2"
+        white--text
+        xs12
+      >
+        &copy;{{ new Date().getFullYear() }}
+        <strong class="ml-2">Бг Ментор</strong>
+      </div>
     </v-layout>
   </v-footer>
 </template>
@@ -23,4 +31,11 @@ export default {
   }
 }
 </script>
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+footer
+  overflow: hidden
+.bg-mentor
+  width: 100%
+  display: flex
+  justify-content: center
+</style>
